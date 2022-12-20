caaefbba46fb4c4b57a1374f0996c118c72b2419
JustOdessa <alexeyvasilenkoo@gmail.com>
2022-12-20 18:53:18 +0200

    Fix for deploy

--- src/config/settings.py
+++ src/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+j5fbjw5rv&nuo%(^f#@3^x^lw!!**5!d1@-hcn4pt%g6a&f08'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["https://qr-code-for-car-owners.azurewebsites.net", "localhost", "127.0.0.1"]
 
 
 # Application definition
