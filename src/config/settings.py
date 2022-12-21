309a873c813f7e8c4f7db489e45ec79bf24cf024
JustOdessa <alexeyvasilenkoo@gmail.com>
2022-12-21 15:55:53 +0200

    Fix for deploy

--- src/config/settings.py
+++ src/config/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*", "https://qr-code-for-car-owners.herokuapp.com"]
+ALLOWED_HOSTS = ["qr-code-for-car-owners.herokuapp.com", "localhost"]
 
 
 # Application definition
