eff88dc44b435e9b16ee2b314f9bef700ba6ea40
JustOdessa <alexeyvasilenkoo@gmail.com>
2022-12-21 18:35:35 +0200

    Fix for deploy

--- src/config/settings.py
+++ src/config/settings.py
@@ -13,6 +13,10 @@ import os
 from pathlib import Path
 import django_heroku
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,6 +133,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Enable WhiteNoise's GZip compression of static assets.
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
