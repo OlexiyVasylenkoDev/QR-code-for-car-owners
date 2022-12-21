4ac25082c50378f4ca5b4a562a899e21002e7407
JustOdessa <alexeyvasilenkoo@gmail.com>
2022-12-21 15:49:20 +0200

    Fix for deploy

--- src/config/settings.py
+++ src/config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'django-insecure-+j5fbjw5rv&nuo%(^f#@3^x^lw!!**5!d1@-hcn4pt%g6a&f08'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
