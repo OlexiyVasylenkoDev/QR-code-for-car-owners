55bf03f8565b0afc48410e21855478caf7e02224
JustOdessa <alexeyvasilenkoo@gmail.com>
2022-12-22 10:15:02 +0200

    Minor fixes for deploy

--- src/core/urls.py
+++ src/core/urls.py
@@ -5,9 +5,9 @@ from core.views import generate_qr, Login, Logout, Registration, QRCodeView, Pro
 app_name = "core"
 
 urlpatterns = [
-    path('', Index.as_view(), name="index"),
+    path('/', Index.as_view(), name="index"),
     path('login/', Login.as_view(), name="login"),
-    path("logout/", Logout.as_view(), name="logout"),
+    path('logout/', Logout.as_view(), name="logout"),
     path('registration/', Registration.as_view(), name="registration"),
     path('profile/', Profile.as_view(), name="profile"),
     path('generate_qr/', generate_qr, name="generate_qr"),
