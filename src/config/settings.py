68665fb4de7fa18563f9bcb60325b5cd06a60860
JustOdessa <alexeyvasilenkoo@gmail.com>
2022-12-22 10:31:22 +0200

    Minor fixes for deploy

--- src/config/settings.py
+++ src/config/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 # WHITENOISE_USE_FINDERS = True
