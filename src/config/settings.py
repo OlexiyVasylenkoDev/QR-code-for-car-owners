d3443fd2190c6dc32511560bdffeaaacfeabad0c
JustOdessa <alexeyvasilenkoo@gmail.com>
2023-01-11 17:32:32 +0200

    Change for local run

--- src/config/settings.py
+++ src/config/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "@!p@n&#m8jd!-#4*+1%_z)=^)ivh1tjg#y-qml5g2@-^g(6w9e"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
