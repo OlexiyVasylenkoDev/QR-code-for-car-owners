eb21f84b35bf2ac52830ba49ec5ef1453b2d011c
JustOdessa <alexeyvasilenkoo@gmail.com>
2023-01-27 18:42:18 +0200

    Minor fix

--- src/config/settings.py
+++ src/config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
-import django_heroku
+# import django_heroku
 from dotenv import load_dotenv
 
 load_dotenv()
